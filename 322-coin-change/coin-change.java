1a6074d419cb7260e3b714a775d13b0694098748
Alisha Kapoor <alishakapoor0907@gmail.com>
2025-12-03 20:57:14 +0530

    Time: 18 ms (40.79%) | Memory: 46.8 MB (17.15%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -2,11 +2,31 @@ class Solution {
     int n;
     int[][] dp;
     public int coinChange(int[] coins, int amount) {
+        //Approach 1 : Bottom Up
         n=coins.length;
+        int INF = amount + 1;
         dp=new int[n+1][amount+1];
-        for(int[] row:dp) Arrays.fill(row,-1);
-        int ans=solve(coins,n,amount);
-        return ans==Integer.MAX_VALUE?-1:ans;
+        for (int j = 1; j <= amount; j++)
+            dp[0][j] = INF;
+
+        for (int i = 1; i <= n; i++)
+            dp[i][0] = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+                if(coins[i-1]<=j && dp[i][j-coins[i-1]]!=INF){
+                    dp[i][j]=Math.min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][amount]==INF?-1:dp[n][amount];
+
+        //Approach 2: Top Down
+        // for(int[] row:dp) Arrays.fill(row,-1);
+        // int ans=solve(coins,n,amount);
+        // return ans==Integer.MAX_VALUE?-1:ans;
     }
     public int solve(int[] coins,int i,int amount){
         if(i==0) return Integer.MAX_VALUE;
