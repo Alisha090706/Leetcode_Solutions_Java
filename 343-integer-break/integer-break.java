a0a6ff1415503fed7120118dc577df415617af2e
Alisha Kapoor <alishakapoor0907@gmail.com>
2025-12-03 20:23:35 +0530

    updated file

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -6,13 +6,20 @@ class Solution {
         return solve(n);
     }
     public int solve(int n){
+        //if n==1 then there is no other way to break it.
         if(n==1) return 1;
+
+        //if the answer is already in dp, then return it there only
         if(dp[n]!=-1) return dp[n];
+        
+        //splitting the number further. Either we will get the max at i*(n-i) or at i*solve(n-i)
         int result=Integer.MIN_VALUE;
         for(int i=1;i<n;i++){
             int prod=i*Math.max(n-i,solve(n-i));
             result=Math.max(prod,result);
         }
+
+        //storing the result in dp
         return dp[n]=result;
     }
 }
