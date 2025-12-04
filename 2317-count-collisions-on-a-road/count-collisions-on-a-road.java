b62075c74de7a9f2c67ccb41082214b0ca273f79
Alisha Kapoor <alishakapoor0907@gmail.com>
2025-12-04 11:35:36 +0530

    Time: 10 ms (93.15%) | Memory: 47.8 MB (18.15%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -1,42 +1,58 @@
 class Solution {
     public int countCollisions(String directions) {
+        //Approach 1 : Stack 
         int n=directions.length();
         int collisions=0;
-        Stack<Character> st=new Stack<>();
-        st.push(directions.charAt(0));
-        for(int i=1;i<n;i++){
-            char c=directions.charAt(i);
-            if(c=='L'){
+        // Stack<Character> st=new Stack<>();
+        // st.push(directions.charAt(0));
+        // for(int i=1;i<n;i++){
+        //     char c=directions.charAt(i);
+        //     if(c=='L'){
 
-                if(!st.isEmpty() && st.peek()=='R'){
-                    collisions+=2;
-                    st.pop();
+        //         if(!st.isEmpty() && st.peek()=='R'){
+        //             collisions+=2;
+        //             st.pop();
 
-                    //remaining right cars keep crashing
-                    while(!st.isEmpty() && st.peek()=='R'){
-                        collisions++;
-                        st.pop();
-                    }
+        //             //remaining right cars keep crashing
+        //             while(!st.isEmpty() && st.peek()=='R'){
+        //                 collisions++;
+        //                 st.pop();
+        //             }
 
-                    st.push('S');
-                }
-                else if(!st.isEmpty() && st.peek()=='S'){
-                    collisions++;
-                    st.push('S');
-                }
-                //safe left
-                else st.push('L');
-            }
-            else if(c=='S'){
-                while(!st.isEmpty() && st.peek()=='R'){
-                    collisions++;
-                    st.pop();
-                }
-                st.push('S');
-            }
-            else{
-                st.push('R');
-            }
+        //             st.push('S');
+        //         }
+        //         else if(!st.isEmpty() && st.peek()=='S'){
+        //             collisions++;
+        //             st.push('S');
+        //         }
+        //         //safe left
+        //         else st.push('L');
+        //     }
+        //     else if(c=='S'){
+        //         while(!st.isEmpty() && st.peek()=='R'){
+        //             collisions++;
+        //             st.pop();
+        //         }
+        //         st.push('S');
+        //     }
+        //     else{
+        //         st.push('R');
+        //     }
+        // }
+
+        //Approach 2 : Two Pointers
+        collisions=0;
+        int i=0;
+        int j=n-1;
+        while(i<n && directions.charAt(i)=='L'){
+            i++;
+        }
+        while(j>=0 && directions.charAt(j)=='R'){
+            j--;
+        }
+        while(i<=j){
+            if(directions.charAt(i)!='S') collisions++;
+            i++;
         }
         return collisions;
         
