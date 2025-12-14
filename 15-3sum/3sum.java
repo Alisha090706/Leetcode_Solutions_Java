f1f5f813c5946d492776ba8298de3e841c52909d
Alisha Kapoor <alishakapoor0907@gmail.com>
2025-12-14 23:24:01 +0530

    Time: 59 ms (20.52%) | Memory: 61.5 MB (5.34%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> result=new ArrayList<>();
+        HashSet<List<Integer>> set=new HashSet<>();
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
+            int target=-nums[i];
+            List<List<Integer>> pairs=twoSum(nums,j,k,target);
+            for(List<Integer> pair:pairs){
+                List<Integer> triplet=new ArrayList<>();
+                triplet.addAll(pair);
+                triplet.add(nums[i]);
+                if(!set.contains(triplet)){
+                    result.add(triplet);
+                    set.add(triplet);
+                }
+            }
+        }
+        return result;
+        
+    }
+    public List<List<Integer>> twoSum(int[] nums,int i,int j,int target){
+        List<List<Integer>> pairs=new ArrayList<>();
+        while(i<j){
+            List<Integer> pair=new ArrayList<>();
+            if(nums[i]+nums[j]==target){
+                pair.add(nums[i]);
+                pair.add(nums[j]);
+                pairs.add(pair);
+                i++;
+                j--;
+            }
+            else if(nums[i]+nums[j]>target) j--;
+            else i++;
+        }
+        return pairs;
+    }
+}
