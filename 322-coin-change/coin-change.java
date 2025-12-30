4c1b4283675b7df6e76702136dcae6c989da2a2c
Alisha Kapoor <alishakapoor0907@gmail.com>
2025-12-30 20:15:04 +0530

    Time: 18 ms (43.61%) | Memory: 47 MB (24.27%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,52 +1,21 @@
 class Solution {
-    int n;
-    int[][] dp;
     public int coinChange(int[] coins, int amount) {
-        //Approach 1 : Bottom Up
-        n=coins.length;
-        int INF = amount + 1;
-        dp=new int[n+1][amount+1];
-        for (int j = 1; j <= amount; j++)
-            dp[0][j] = INF;
+        int n=coins.length;
+        int[][] dp=new int[n+1][amount+1];
+        int INF=amount+1;
+        for(int i=1;i<=amount;i++) dp[0][i]=INF;
 
-        for (int i = 1; i <= n; i++)
-            dp[i][0] = 0;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=amount;j++){
-                if(coins[i-1]<=j && dp[i][j-coins[i-1]]!=INF){
-                    dp[i][j]=Math.min(1+dp[i][j-coins[i-1]],dp[i-1][j]);
+            for(int sum=1;sum<=amount;sum++){
+                if(sum>=coins[i-1] && dp[i][sum-coins[i-1]]!=INF){
+                    dp[i][sum]=Math.min(dp[i-1][sum],1+dp[i][sum-coins[i-1]]);
                 }
                 else{
-                    dp[i][j]=dp[i-1][j];
+                    dp[i][sum]=dp[i-1][sum];
                 }
             }
         }
         return dp[n][amount]==INF?-1:dp[n][amount];
-
-        //Approach 2: Top Down
-        // for(int[] row:dp) Arrays.fill(row,-1);
-        // int ans=solve(coins,n,amount);
-        // return ans==Integer.MAX_VALUE?-1:ans;
-    }
-    public int solve(int[] coins,int i,int amount){
-        if(i==0) return Integer.MAX_VALUE;
-        if(amount==0) return 0;
-
-        if(dp[i][amount]!=-1) return dp[i][amount];
-
-        //take the current coin
-        int take=Integer.MAX_VALUE;
-        if(coins[i-1]<=amount){
-            //overflow condition, hence take care of it
-            int next=solve(coins,i,amount-coins[i-1]);
-            if(next!=Integer.MAX_VALUE){
-                take=1+next;
-            }
-        }
-
-        //skip
-        int skip=solve(coins,i-1,amount);
-
-        return dp[i][amount]=Math.min(take,skip);
+        
     }
 }
