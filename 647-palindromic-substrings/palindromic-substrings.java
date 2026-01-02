5b8bad4f3c6cacd51392ce096d0bfa38815d9d8a
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-02 18:43:26 +0530

    Time: 21 ms (25.90%) | Memory: 46.6 MB (22.31%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,25 +1,36 @@
 class Solution {
     int n;
-    Boolean[][] dp;
+    // Boolean[][] dp;
     public int countSubstrings(String s) {
         n=s.length();
-        dp=new Boolean[n][n];
+        boolean[][] dp=new boolean[n][n];
         int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(checkpalindrome(s,i,j)){
-                    count++;
+        for(int L=1;L<=n;L++){
+            for(int i=0;i+L-1<n;i++){
+                int j=i+L-1;
+                if(i==j){
+                    dp[i][j]=true;
                 }
+               else if(i+1==j){
+                    dp[i][j]=s.charAt(i)==s.charAt(j);
+                }
+                else{
+                    dp[i][j]=(s.charAt(i)==s.charAt(j)) && dp[i+1][j-1];
+                }
+                if(dp[i][j]) count++;
             }
         }
+        
         return count;
     }
-    public boolean checkpalindrome(String s,int i,int j){
-        if(i>j) return true;
-        if(dp[i][j]!=null) return dp[i][j];
-        if(s.charAt(i)==s.charAt(j)){
-            return dp[i][j]=checkpalindrome(s,i+1,j-1);
-        }
-        return dp[i][j]=false;
-    }
+
+    // Approach 2: Memoization
+    // public boolean checkpalindrome(String s,int i,int j){
+    //     if(i>j) return true;
+    //     if(dp[i][j]!=null) return dp[i][j];
+    //     if(s.charAt(i)==s.charAt(j)){
+    //         return dp[i][j]=checkpalindrome(s,i+1,j-1);
+    //     }
+    //     return dp[i][j]=false;
+    // }
 }
