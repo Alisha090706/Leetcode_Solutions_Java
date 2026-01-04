530131cc39574da0206e200d6f75f0f2d3126439
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-04 21:37:33 +0530

    Time: 18 ms (89.93%) | Memory: 47.1 MB (65.58%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -2,20 +2,36 @@ class Solution {
     int[][] dp;
     public int minInsertions(String s) {
         int n = s.length();
+        if(n == 1) return 0;
         dp = new int[n][n];
-        for(int[] row : dp) Arrays.fill(row,-1);
-
-        return solve(s,0,n-1);
-    }
-    public int solve(String s, int i, int j){
-        if(i > j) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s.charAt(i) == s.charAt(j)){
-            return dp[i][j] = solve(s, i+1, j-1);
+        for(int L = 2; L <= n; L++){
+            for(int i = 0; i + L -1 < n; i++){
+                int j = i+L-1;
+                if(s.charAt(i) == s.charAt(j)){
+                    dp[i][j] = dp[i+1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + Math.min(dp[i+1][j],dp[i][j-1]);
+                }
+            }
         }
 
-        return dp[i][j] =1 + Math.min(solve(s, i+1, j), solve(s, i, j-1));
+        return dp[0][n-1];
+
+        // for(int[] row : dp) Arrays.fill(row,-1);
+        // return solve(s,0,n-1);
     }
+
+    // Approach 2: Memoization
+    // public int solve(String s, int i, int j){
+    //     if(i > j) return 0;
+
+    //     if(dp[i][j] != -1) return dp[i][j];
+
+    //     if(s.charAt(i) == s.charAt(j)){
+    //         return dp[i][j] = solve(s, i+1, j-1);
+    //     }
+
+    //     return dp[i][j] =1 + Math.min(solve(s, i+1, j), solve(s, i, j-1));
+    // }
 }
