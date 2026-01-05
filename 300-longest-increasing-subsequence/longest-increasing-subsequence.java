b3ced5229726a8653c0cdb147f99bbdec4556d50
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-05 21:59:50 +0530

    Time: 53 ms (30.70%) | Memory: 45.7 MB (94.67%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,26 +1,41 @@
 class Solution {
     int n;
-    int[][] dp;
+    // int[][] dp;
     public int lengthOfLIS(int[] nums) {
         n = nums.length;
-        dp = new int[n][n];
-        for(int[] row : dp) Arrays.fill(row, -1);
-        return solve(nums,0,-1);
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);
+        // Aprroach : Bottom Up
+        int len = 1;
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i]){
+                    dp[i] = Math.max(dp[i],dp[j] + 1);
+                    len = Math.max(len, dp[i]);
+                }
+            }
+        }
+        return len;
+        // for(int[] row : dp) Arrays.fill(row, -1);
+        // return solve(nums,0,-1);
     }
-    public int solve(int[] nums,int i,int prev){
-        if(i == n){
-            return 0;
-        }
-        if(prev != -1 && dp[i][prev] != -1) return dp[i][prev];
-        int take = 0;
-        if(prev==-1 || nums[prev]<nums[i]){
-            take =1 + solve(nums,i+1,i);
-        }
-        int skip = solve(nums,i+1,prev);
+
+    // Approach : Memoization
+    // public int solve(int[] nums,int i,int prev){
+    //     if(i == n){
+    //         return 0;
+    //     }
+    //     if(prev != -1 && dp[i][prev] != -1) return dp[i][prev];
+    //     int take = 0;
+    //     if(prev==-1 || nums[prev]<nums[i]){
+    //         take =1 + solve(nums,i+1,i);
+    //     }
+    //     int skip = solve(nums,i+1,prev);
         
-        if(prev != -1){
-            dp[i][prev] = Math.max(take,skip);
-        }
-        return Math.max(take,skip);
-    }
+    //     if(prev != -1){
+    //         dp[i][prev] = Math.max(take,skip);
+    //     }
+    //     return Math.max(take,skip);
+    // }
 }
