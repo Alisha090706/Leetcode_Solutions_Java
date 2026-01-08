1ab00586d5a55a52b288ca57d5f73ef75aff5683
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-08 13:34:35 +0530

    Time: 0 ms (100.00%) | Memory: 45.6 MB (13.13%) - LeetSync

---
+++ 174-dungeon-game/dungeon-game.java
@@ -0,0 +1,28 @@
+class Solution {
+    int m;
+    int n;
+    int[][] dp;
+    public int calculateMinimumHP(int[][] dungeon) {
+        m = dungeon.length;
+        n = dungeon[0].length;
+        dp = new int[m][n];
+        for(int[] row: dp) Arrays.fill(row , -1);
+        return solve(dungeon, 0, 0);
+    }
+    public int solve(int[][] grid, int i, int j){
+        if(i==m-1 && j == n-1){
+            if(grid[i][j] <=0){
+                return Math.abs(grid[i][j]) + 1;
+            }
+            else return 1;
+        }
+        if(i >= m || j >= n) return 1000000000;
+        if(dp[i][j] != -1) return dp[i][j];
+        int down =  solve(grid, i+1, j);
+        int right = solve(grid, i, j+1);
+        int curr = grid[i][j] <= 0? Math.abs(grid[i][j]) : -grid[i][j];
+        int result = curr + Math.min(down, right);
+        return dp[i][j] = (result)<=0? 1 : result;
+    }
+    
+}
