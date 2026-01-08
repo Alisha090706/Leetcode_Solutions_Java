b19a85d03cfa2dfda7d837e963dbbafe5ca6eb4d
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-08 18:17:42 +0530

    Time: 18 ms (59.34%) | Memory: 249 MB (97.51%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.java
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.java
@@ -4,12 +4,48 @@ class Solution {
     public int maxCollectedFruits(int[][] fruits) {
         n = fruits.length;
         dp = new int[n][n];
-        for(int[] row: dp)Arrays.fill(row, -1);
-        int child_one = child1Collect(fruits);
-        int child_two = child2Collect(fruits,0,n-1,0);
-        int child_three = child3Collect(fruits,n-1,0,0);
+        for (int i = 0; i < n; i++)
+            dp[i] = fruits[i].clone();
 
-        return child_one + child_two + child_three;
+        int result = child1Collect(fruits);
+        for(int i = 0; i<n; i++){
+            for(int j=0;j<n;j++){
+                int val =fruits[i][j];
+                if(i<j && i+j<n-1){
+                    dp[i][j]=0;
+                }
+                else if(i>j && i+j<n-1){
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        //child2 collects
+        for(int i=1;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int a = dp[i-1][j];
+                int b = dp[i-1][j-1];
+                int c = (j+1<n)?dp[i-1][j+1]:0;
+                dp[i][j]+=Math.max(a,Math.max(b,c));
+            }
+        }
+        //child3 collects
+        for(int j=1;j<n;j++){
+            for(int i=j+1;i<n;i++){
+                int a = dp[i-1][j-1];
+                int b = dp[i][j-1];
+                int c = i+1<n?dp[i+1][j-1]:0;
+                dp[i][j]+=Math.max(a,Math.max(b,c));
+            }
+        }
+        return result+dp[n-2][n-1]+dp[n-1][n-2];
+
+        
+        // for(int[] row: dp)Arrays.fill(row, -1);
+        // int child_one = child1Collect(fruits);
+        // int child_two = child2Collect(fruits,0,n-1,0);
+        // int child_three = child3Collect(fruits,n-1,0,0);
+
+        // return child_one + child_two + child_three;
     }
     public int child1Collect(int[][] fruits){
         int result = 0;
