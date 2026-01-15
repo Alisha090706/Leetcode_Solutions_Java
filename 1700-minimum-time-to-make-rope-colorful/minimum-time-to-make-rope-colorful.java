abc3e2b82f01a41e672a004a9bfe7aa88b9d961c
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-15 21:07:58 +0530

    Time: 13 ms (11.23%) | Memory: 90.6 MB (68.89%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.java
@@ -5,12 +5,11 @@ class Solution {
         int curr = 1;
         int time = 0;
         while(curr < n){
-            while(curr < n && colors.charAt(curr) == colors.charAt(prev)){
+            if(curr < n && colors.charAt(curr) == colors.charAt(prev)){
                 time += (Math.min(neededTime[prev],neededTime[curr]));
                 if(neededTime[curr] > neededTime[prev]){
                     prev = curr;
                 }
-                curr ++;
             }
             if(curr < n && colors.charAt(prev) != colors.charAt(curr)){
                 prev = curr;
