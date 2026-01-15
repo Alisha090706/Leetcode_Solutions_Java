59ddf0672673fc48e660461c0f3cd365dac4faa8
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-15 20:56:59 +0530

    Time: 1965 ms (5.05%) | Memory: 42.4 MB (9.50%) - LeetSync

---
+++ 1033-broken-calculator/broken-calculator.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int brokenCalc(int startValue, int target) {
+        int steps = 0;
+        while(target != startValue){
+            if(target % 2 == 0 && target > startValue){
+                target /= 2;
+            }
+            else target += 1;
+            steps++;
+        }
+        return steps;
+    }
+}
