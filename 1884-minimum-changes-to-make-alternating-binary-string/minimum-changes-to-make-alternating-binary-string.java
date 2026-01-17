6ccc3442f96146432fede1b43db0b22507027ad4
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-17 16:55:42 +0530

    Time: 3 ms (92.13%) | Memory: 43.8 MB (55.90%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minOperations(String s) {
+        int n = s.length();
+        int start1 = 0;
+        int start0 = 0;
+        for(int i = 0; i < n; i++){
+            if(i%2 == 0){
+                if(s.charAt(i) == '0') start1++;
+                else start0 ++;
+            }
+            else{
+                if(s.charAt(i) == '1') start1++;
+                else start0++;
+            }
+        }
+        return Math.min(start1,start0);
+    }
+        
+}
