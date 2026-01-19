fc033b2476eba39505a96e7cb86d2a5445ee7949
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-19 22:53:23 +0530

    Time: 2 ms (98.11%) | Memory: 50.2 MB (52.31%) - LeetSync

--- 687-longest-univalue-path/longest-univalue-path.java
+++ 687-longest-univalue-path/longest-univalue-path.java
@@ -17,21 +17,22 @@ class Solution {
     int max = 0;
     public int longestUnivaluePath(TreeNode root) {
         if(root == null) return 0;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            TreeNode curr = q.poll();
-            solve(curr,curr.val);
-            if(curr.left != null) q.add(curr.left);
-            if(curr.right != null) q.add(curr.right);
-        }
-        return max - 1;
+        dfs(root);
+        return max;
     }
-    public int solve(TreeNode curr, int val){
-        if(curr == null || curr.val != val) return 0;
-        int left = solve(curr.left,val);
-        int right = solve(curr.right, val);
-        max = Math.max(max, 1 + left + right);
-        return 1 + Math.max(left, right);
+    public int dfs(TreeNode root){
+        if(root == null) return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        int leftLength = 0;
+        int rightLength = 0;
+        if(root.left != null && root.left.val == root.val){
+            leftLength = left + 1;
+        }
+        if(root.right != null && root.right.val == root.val){
+            rightLength = right + 1;
+        }
+        max = Math.max(max, leftLength + rightLength);
+        return Math.max(leftLength, rightLength);
     }
 }
