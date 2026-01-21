b7a41e7c3fe304109e85832ebd20e0e586b6e23f
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-21 12:39:17 +0530

    Time: 3 ms (99.72%) | Memory: 47.6 MB (8.67%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,31 +1,36 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<numCourses;i++){
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++){
             adj.add(new ArrayList<>());
         }
-        for(int[] prerequisite:prerequisites){
-            int u=prerequisite[0];
-            int v=prerequisite[1];
-            adj.get(u).add(v);
+        for(int[] preq : prerequisites){
+            int u = preq[0];
+            int v = preq[1];
+            adj.get(v).add(u);
         }
-        boolean[] visited=new boolean[numCourses];
-        boolean[] inrec=new boolean[numCourses];
-        
-        for(int i=0;i<numCourses;i++){
-            if(!visited[i] && dfs(adj,i,visited,inrec)) return false;
+        boolean[] visited = new boolean[numCourses];
+        boolean[] inRecur = new boolean[numCourses];
+        for(int i = 0; i < numCourses; i++){
+            if(!visited[i] && dfs(adj,i,visited,inRecur)){
+                return false;
+            }
         }
         return true;
     }
-    public boolean dfs(ArrayList<ArrayList<Integer>> adj,int u,boolean[] visited, boolean[] inrec){
-        visited[u]=true;
-        inrec[u]=true;
-        
-        for(int v:adj.get(u)){
-            if(!visited[v] && dfs(adj,v,visited,inrec))return true;
-            else if(inrec[v]) return true;
+    public boolean dfs(ArrayList<ArrayList<Integer>> adj, int u, boolean[] visited, boolean[] inRecur){
+        visited[u] =true;
+        inRecur[u] = true;
+        for(int v: adj.get(u)){
+            
+            if(!visited[v]){
+                if(dfs(adj, v, visited, inRecur)) return true;
+            }
+            else if(inRecur[v]){
+                return true;
+            }
         }
-        inrec[u]=false;
+        inRecur[u] = false;
         return false;
     }
 }
