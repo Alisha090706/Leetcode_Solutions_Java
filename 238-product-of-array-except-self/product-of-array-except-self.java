3cb65e2a8d86c5696c23632977c6bc70d21da4e3
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-22 21:24:40 +0530

    Time: 2 ms (94.30%) | Memory: 64 MB (94.87%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,25 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] result = new int[n];
-        int[] prefix = new int[n];
-        prefix[0]= nums[0];
+        int[] prefix = new int[n]; // product till index i
+        prefix[0] = nums[0];
         for(int i = 1; i < n; i++){
             prefix[i] = prefix[i-1] * nums[i];
-        }
-        int[] suffix = new int[n];
-        suffix[n-1] = nums[n-1];
-        for(int i = n-2; i >= 0; i--){
-            suffix[i]= nums[i] * suffix[i+1];
-        }
-        for(int i = 0; i < n ;i++){
-            if(i == 0) result[i]= suffix[i+1];
-            else if(i==n-1) result[i] = prefix[i-1];
-            else{
-                result[i] = prefix[i-1] *suffix[i+1];
-            }
-        }
-        return result;
+        } 
 
+        // product except itself == prefix[i-1] * suffix[i+1];
+        int suffix = 1;
+        int[] result = new int[n];
+        for(int i = n - 1; i > 0; i--){
+            result[i] = prefix[i-1] *suffix;
+            suffix *= nums[i];
+        }
+        result[0] = suffix;
+        return result;
     }
 }
