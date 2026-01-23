96245d6f6552c9c5cc7e9ee9b110d8ce131a972e
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-23 12:34:54 +0530

    Time: 42 ms (23.14%) | Memory: 60.8 MB (5.53%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,20 +1,36 @@
 class Solution {
-    
     public List<List<Integer>> threeSum(int[] nums) {
-        int n=nums.length;
+        int n = nums.length;
+
+        //creating a result arraylist
+        List<List<Integer>> result = new ArrayList<>();
+        
+        //sorting the array as we only need to check in the array after the index i.
         Arrays.sort(nums);
-        List<List<Integer>> result=new ArrayList<>();
-        HashSet<List<Integer>> set=new HashSet<>();
-        for(int i=0;i<n;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int j=i+1;
-            int k=n-1;
-            int target=-nums[i];
-            List<List<Integer>> pairs=twoSum(nums,j,k,target);
-            for(List<Integer> pair:pairs){
-                List<Integer> triplet=new ArrayList<>();
-                triplet.addAll(pair);
-                triplet.add(nums[i]);
+
+        //We can convert 3 sum to 2 sum -> nums[i] + nums[j] + nums[k] = 0;
+        // nums[i] + nums[j] = -nums[k] -> -nums[k] is the target here.
+
+        //We will require a set to ensure that no triplet is repeated again due to duplicates.
+        HashSet<List<Integer>> set = new HashSet<>();
+
+        for(int k = 0; k < n - 2; k++){
+            //checking if the curr element is already processed or not. 
+            // if already processed we can just skip it.
+            if(k > 0 && nums[k] == nums[k - 1]) continue;
+
+            int num1 = nums[k];
+            int target = -num1;
+
+            int i = k + 1;
+            int j = n - 1;
+
+            List<List<Integer>> doublets = twoSum(nums, i, j, target);
+
+            for(List<Integer> doublet : doublets){
+                List<Integer> triplet = new ArrayList<>();
+                triplet.add(num1);
+                triplet.addAll(doublet);
                 if(!set.contains(triplet)){
                     result.add(triplet);
                     set.add(triplet);
@@ -22,22 +38,28 @@ class Solution {
             }
         }
         return result;
-        
     }
-    public List<List<Integer>> twoSum(int[] nums,int i,int j,int target){
-        List<List<Integer>> pairs=new ArrayList<>();
-        while(i<j){
-            List<Integer> pair=new ArrayList<>();
-            if(nums[i]+nums[j]==target){
-                pair.add(nums[i]);
-                pair.add(nums[j]);
-                pairs.add(pair);
+    public List<List<Integer>> twoSum(int[] nums,int i ,int j, int target){
+        List<List<Integer>> doublet = new ArrayList<>();
+        
+        while(i < j){
+            int num1 = nums[i];
+            int num2 = nums[j];
+            if(num1 + num2 == target){
+                List<Integer> doub = new ArrayList<>();
+                doub.add(num1);
+                doub.add(num2);
+                doublet.add(doub);
                 i++;
                 j--;
             }
-            else if(nums[i]+nums[j]>target) j--;
-            else i++;
+            else if(num1 + num2 < target){
+                i++;
+            }
+            else{
+                j--;
+            }
         }
-        return pairs;
+        return doublet;
     }
 }
