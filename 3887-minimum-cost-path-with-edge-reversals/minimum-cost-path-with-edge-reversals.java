e3e4c1eec25dd4dcc53863e6a74410b0fa086c30
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-01-27 22:16:06 +0530

    Time: 134 ms (42.51%) | Memory: 196.3 MB (94.74%) - LeetSync

---
+++ 3887-minimum-cost-path-with-edge-reversals/minimum-cost-path-with-edge-reversals.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minCost(int n, int[][] edges) {
+        List<List<int[]>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge: edges){
+            int u = edge[0];
+            int v = edge[1];
+            int d = edge[2];
+            adj.get(u).add(new int[]{v,d});
+            adj.get(v).add(new int[]{u,2*d});
+        }
+        int[] cost = new int[n];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+
+        q.add(new int[]{0,0});
+        cost[0] = 0;
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int u = curr[0];
+            int dist = curr[1];
+            for(int[] neigh : adj.get(u)){
+                int v = neigh[0];
+                int d = neigh[1];
+                if(d + dist < cost[v]){
+                    cost[v] = d + dist;
+                    q.add(new int[]{v,cost[v]});
+                    
+                }
+            }
+        }
+        return cost[n-1] == Integer.MAX_VALUE? -1 : cost[n-1];
+    }
+
+}
