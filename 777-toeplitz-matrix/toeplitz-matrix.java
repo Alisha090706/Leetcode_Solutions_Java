16d44875ca9d815393bba9c4db3b4b5f10a5c80a
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-02-09 16:07:14 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (38.98%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.java
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -3,14 +3,8 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         for(int row = 0; row < m-1; row++){
-            for(int i = 0; i < n-1; i++){
-                int curr = matrix[row][i];
-                int col = i;
-                for(int j = row; j < m; j++){
-                    if(col == n) break;
-                    if(matrix[j][col] != curr)return false;
-                    col++;
-                }
+            for(int col = 0; col < n-1; col++){
+                if(matrix[row][col] != matrix[row+1][col+1])return false;
             }
         }
         return true;
