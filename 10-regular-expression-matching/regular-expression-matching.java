67f80d7314d0ce3a191129ff5f248f3a34091510
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-02-11 11:56:55 +0530

    Time: 1 ms (99.70%) | Memory: 44 MB (16.22%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,14 +1,17 @@
 class Solution {
     int m;
     int n;
+    Boolean[][] dp;
     public boolean isMatch(String s, String p) {
         m = s.length();
         n = p.length();
+        dp = new Boolean[m+1][n+1];
         return solve(s, p, 0, 0);
     }
     public boolean solve(String s, String p, int i, int j){
         if(j == n) return i == m;
         
+        if(dp[i][j] != null) return dp[i][j];
         //check if current matches
         boolean firstmatch = (i < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
         //check if next is "*"
@@ -20,8 +23,8 @@ class Solution {
 
             boolean skip = solve(s, p, i, j + 2);
             boolean use = firstmatch && solve(s, p, i+1, j);
-            return skip || use;
+            return dp[i][j] = skip || use;
         }
-        return firstmatch && solve(s, p, i+1, j+1);
+        return dp[i][j] = firstmatch && solve(s, p, i+1, j+1);
     }
 }
