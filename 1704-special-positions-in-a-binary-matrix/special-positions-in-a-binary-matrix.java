eda328bf179ee3aaa0faefeaadb5af6f299a1e7a
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-03-04 23:55:02 +0530

    Time: 2 ms (94.84%) | Memory: 47 MB (83.45%) - LeetSync

--- 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
+++ 1704-special-positions-in-a-binary-matrix/special-positions-in-a-binary-matrix.java
@@ -2,33 +2,25 @@ class Solution {
     public int numSpecial(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
+
+        int[] row = new int[m];
+        int[] col = new int[n];
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 1){
+                    row[i] ++;
+                    col[j] ++;
+                }
+            }
+        }
+        
         int count = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(mat[i][j] == 0) continue;
-                boolean rowSafe =true;
-                boolean colSafe =true;
-                for(int row = 0; row < m; row++){
-                    if(row == i) continue;
-                    if(mat[row][j] == 1){
-                        colSafe = false;
-                        break;
-                    }
-                }
-                for(int col = 0; col < n; col++) {
-                    if(col == j) continue;
-                    if(mat[i][col] == 1){
-                        rowSafe = false;
-                        break;
-                    }
-                }
-                if(rowSafe && colSafe) {
-                    count++;
-                }
-                break;
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1) count++;
             }
         }
         return count;
-
     }
 }
