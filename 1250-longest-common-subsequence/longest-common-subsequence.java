2d6bcda443b70a16736926325faa14b203fe487c
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-03-05 21:25:33 +0530

    Time: 21 ms (56.33%) | Memory: 54.1 MB (50.56%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -16,14 +16,33 @@ class Solution {
                 }
             }
         }
+        int i = m;
+        int j = n;
+        StringBuilder sb = new StringBuilder();
+        while(i > 0 && j > 0){
+            if(text1.charAt(i - 1) == text2.charAt(j - 1)){
+                sb.append(text2.charAt(j - 1));
+                i--;
+                j--;
+            }
+            else{
+                if(dp[i - 1][j] > dp[i][j - 1]){
+                    i--;
+                }
+                else{
+                    j--;
+                }
+            }
+        }
+        System.out.println(sb.reverse().toString());
         return dp[m][n];
     }
-    public int solve(String s, String p, int i, int j){
-        if(i < 0 || j < 0) return 0;
-        if(s.charAt(i) == p.charAt(j)){
-            return dp[i][j] = 1 + solve(s,p,i-1,j-1);
-        }
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = Math.max(solve(s,p,i-1,j), solve(s,p,i,j-1));
-    }
+    // public int solve(String s, String p, int i, int j){
+    //     if(i < 0 || j < 0) return 0;
+    //     if(s.charAt(i) == p.charAt(j)){
+    //         return dp[i][j] = 1 + solve(s,p,i-1,j-1);
+    //     }
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     return dp[i][j] = Math.max(solve(s,p,i-1,j), solve(s,p,i,j-1));
+    // }
 }
