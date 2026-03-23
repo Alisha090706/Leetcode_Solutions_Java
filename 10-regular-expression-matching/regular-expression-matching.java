5ed51b8876e720ff1306b9af41cf515d6c463f6b
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-03-23 21:24:48 +0530

    Time: 2 ms (54.27%) | Memory: 43.9 MB (24.92%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -1,30 +1,32 @@
 class Solution {
-    int m;
-    int n;
-    Boolean[][] dp;
     public boolean isMatch(String s, String p) {
-        m = s.length();
-        n = p.length();
-        dp = new Boolean[m+1][n+1];
-        return solve(s, p, 0, 0);
-    }
-    public boolean solve(String s, String p, int i, int j){
-        if(j == n) return i == m;
-        
-        if(dp[i][j] != null) return dp[i][j];
-        //check if current matches
-        boolean firstmatch = (i < m && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
-        //check if next is "*"
-        char next = (j + 1 < n) ? p.charAt(j+1):'$';
-        if(next == '*'){
-            //two choices
-            //skip
-            //use
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
 
-            boolean skip = solve(s, p, i, j + 2);
-            boolean use = firstmatch && solve(s, p, i+1, j);
-            return dp[i][j] = skip || use;
+        // handle patterns like a*, a*b*, etc
+        for (int j = 2; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
         }
-        return dp[i][j] = firstmatch && solve(s, p, i+1, j+1);
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++){
+                if(s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if(p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    char prev = p.charAt(j - 2);
+
+                    // one or more occurrence
+                    if (prev == s.charAt(i - 1) || prev == '.') {
+                        dp[i][j] |= dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
