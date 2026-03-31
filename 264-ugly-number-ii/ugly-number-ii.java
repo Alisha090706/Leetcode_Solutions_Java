ec33ceaa151e1960e56690cc3a50fd0e405cfb12
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-03-31 13:22:49 +0530

    Time: 3 ms (38.80%) | Memory: 44.2 MB (36.40%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.java
+++ 264-ugly-number-ii/ugly-number-ii.java
@@ -1,33 +1,19 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        PriorityQueue<Long> q = new PriorityQueue<>();
-        HashSet<Long> set = new HashSet<>();
-        if(n == 1) return n;
-        int un = 1;
-        q.add(1L);
-        while(!q.isEmpty()) {
-            long curr = q.poll();
-            if(un == n) return (int)curr;
-            long a = curr * 2;
-            long b = curr * 3;
-            long c = curr * 5;
-            if(!set.contains(a)) {
-                set.add(a);
-                q.add(a);
-            }
-            if(!set.contains(b)) {
-                set.add(b);
-                q.add(b);
-            }
-            if(!set.contains(c)) {
-                set.add(c);
-                q.add(c);
-            }
-            
-            un++;
+        long[] dp = new long[n];
+        dp[0] = 1;
+
+        int i2 = 0, i3 = 0, i5 = 0;
+        for(int i = 1; i < n; i++) {
+            long a = dp[i2] * 2;
+            long b = dp[i3] * 3;
+            long c = dp[i5] * 5;
+
+            dp[i] = Math.min(a, Math.min(b, c));
+            if(dp[i] == a) i2++;
+            if(dp[i] == b) i3++;
+            if(dp[i] == c) i5++;
         }
-        return -1;
-
-
+        return (int) dp[n - 1];
     }
 }
