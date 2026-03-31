c6fce5c436512c42fb89a7705c4c83655d1a3453
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-03-31 13:44:23 +0530

    Time: 37 ms (43.81%) | Memory: 44.5 MB (58.04%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,21 +1,13 @@
 class Solution {
-    int[][] dp;
     public int numSquares(int n) {
-        dp = new int[n + 1][(int) Math.sqrt(n) + 1];
-        for(int[] row: dp) Arrays.fill(row , -1);
-        return solve(n, 1);
-    }
-    public int solve(int n, int i) {
-        if(n == 0) return 0;
-        if(n < 0 || i * i > n) return 100000;
-        if(dp[n][i] != -1) return dp[n][i];
-        //not take 
-        int nottake = solve(n, i + 1);
-
-        //int take
-        int takeAgain = 1 + solve(n - (i * i), i);
-        int takeOnce = 1 + solve(n - (i * i), i + 1);
-
-        return dp[n][i] = Math.min(nottake, Math.min(takeAgain, takeOnce));
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j * j <= i; j++){
+                dp[i] = Math.min(dp[i], 1 + dp[i - j * j]);
+            }
+        }
+        return dp[n];
     }
 }
