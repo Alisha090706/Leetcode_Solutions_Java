7427f3e29ff30aaf1a28e612efaef5126fb8ea42
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-05-04 12:39:50 +0530

    Time: 3 ms (40.65%) | Memory: 46.8 MB (28.07%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,53 +1,50 @@
 class Solution {
-    int N;
     List<List<String>> result = new ArrayList<>();
+    int N;
     public List<List<String>> solveNQueens(int n) {
         N = n;
-        char[][] board = new char[N][N];
-        for(char[] row: board)Arrays.fill(row, '.');
-        solve(n, 0, board);
+        char[][] board = new char[n][n];
+        for(char[] row: board) Arrays.fill(row, '.');
+        solve(board, 0, n);
         return result;
     }
-    public void solve(int n, int col, char[][] board){
-        if(n == 0){
+    public void solve(char[][] board, int col, int queens) {
+        if(queens == 0) {
             result.add(construct(board));
             return;
         }
         if(col >= N) return;
-        for(int row = 0; row < N; row++){
-            
-            if(isValid(board,row,col)){
-                board[row][col] = 'Q';
-                solve(n-1, col + 1,board);
-                board[row][col] = '.';
+
+        for(int i = 0; i < N ;i++) {
+            if(isValid(board,i,col)) {
+                board[i][col] = 'Q';
+                solve(board, col + 1, queens - 1);
+                board[i][col] = '.';
             }
-            
         }
     }
-    public boolean isValid(char[][] board, int row, int col){
-        for(int i = 0; i < N; i++){
-            if(board[row][i] == 'Q') return false;
+    public boolean isValid(char[][] board, int row, int col) {
+        for(int i = 0; i < N; i++) {
+            if(board[row][i] == 'Q' || board[i][col] == 'Q') return false;
         }
         //upper diagonal
-        int i = row - 1;
-        int j = col - 1;
-        while(i >= 0 && j >= 0){
+        int i = row;
+        int j = col;
+        while(i >= 0 && j >= 0) {
             if(board[i][j] == 'Q') return false;
             i--;
             j--;
         }
 
         //lower diagonal
-        i = row + 1;
-        j = col - 1;
-        while(i < N && j >= 0){
+        i = row;
+        j = col;
+        while(i < N && j >= 0) {
             if(board[i][j] == 'Q') return false;
             i++;
             j--;
-        } 
-
+        }
         return true;
-
     }
     public ArrayList<String> construct(char[][] board){
         ArrayList<String> output=new ArrayList<>();
