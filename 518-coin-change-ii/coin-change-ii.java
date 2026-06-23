e699d55f86868ae72f3704e5d5314e3f9d521b81
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-06-23 21:13:11 +0530

    Time: 18 ms (24.23%) | Memory: 57.2 MB (26.92%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,20 +1,19 @@
 class Solution {
+    int[][] dp;
     public int change(int amount, int[] coins) {
-        int n=coins.length;
-        int[][] dp=new int[n+1][amount+1];
-        for(int i=0;i<=n;i++)dp[i][0]=1;
+        int n = coins.length;
+        dp = new int[n][amount + 1];
+        for(int[] row: dp) Arrays.fill(row , -1);
+        return solve(coins, coins.length - 1, amount);
+    }
+    public int solve(int[] coins, int i, int amount) {
+        if(amount < 0 || i < 0) return 0;
+        if(amount == 0) return 1;
 
-        for(int i=1;i<=n;i++){
-            for(int sum=1;sum<=amount;sum++){
-                if(sum>=coins[i-1]){
-                    dp[i][sum]=dp[i-1][sum]+dp[i][sum-coins[i-1]];
-                }
-                else{
-                    dp[i][sum]=dp[i-1][sum];
-                }
-            }
-        }
-        return dp[n][amount];
-        
+        if(dp[i][amount] != -1) return dp[i][amount];
+        int takeSameCoin = solve(coins, i, amount - coins[i]);
+        int moveToNextCoin = solve(coins, i - 1, amount);
+
+        return dp[i][amount] = takeSameCoin + moveToNextCoin;
     }
 }
