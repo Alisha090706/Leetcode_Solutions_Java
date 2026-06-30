6c7ede4b93cca38ea4f6dd957a968ec4884a40f8
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-06-30 14:52:25 +0530

    Time: 36 ms (8.43%) | Memory: 104 MB (5.05%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -3,27 +3,25 @@ class Solution {
         Arrays.sort(nums);
         int n = nums.length;
         int max = 1;
-        int[] prefix = new int[n];
+        long[] prefix = new long[n];
         prefix[0] = nums[0];
         for(int i = 1; i < n; i++) {
             prefix[i] = nums[i] + prefix[i - 1];
         }
-        for(int i = 1; i < n; i++) {
-            int left = 0;
-            int right = i;
-            while(left <= right) {
-                int mid = left + (right - left) / 2;
-                int expsum = (i - mid + 1) * nums[i];
-                int sum = prefix[i] - (mid - 1 >= 0 ? prefix[mid - 1] : 0);
-                if(expsum - sum <= k) {
-                    max = Math.max(max, i - mid + 1);
-                    right = mid - 1;
-                }
-
-                else {
-                    left = mid + 1;
-                }
+        int i = 0;
+        int j = 0;
+        while(j < n) {
+            int window = j - i + 1;
+            long maxSum = (long)window * nums[j];
+            long sum = prefix[j] - (i > 0 ? prefix[i - 1] : 0);
+            while(i <= j && maxSum - sum > k) {
+                sum -= nums[i];
+                i++;
+                maxSum = (long)(j - i + 1) * nums[j];
             }
+            max = Math.max(max, j - i + 1);
+            j++;
+            
         }
         return max;
     }
