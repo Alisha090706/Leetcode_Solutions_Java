3c67e2043a5fb955ec5e5e33e9810ce7884a6bfe
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-08-17 11:50:10 +0530

    Time: 24 ms (34.09%) | Memory: 54.2 MB (20.82%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -5,44 +5,17 @@ class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         m = text1.length();
         n = text2.length();
-        dp = new int[m+1][n+1];
-        for(int i = 1; i <= m; i++){
-            for(int j = 1; j <= n; j++){
-                if(text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-        int i = m;
-        int j = n;
-        StringBuilder sb = new StringBuilder();
-        while(i > 0 && j > 0){
-            if(text1.charAt(i - 1) == text2.charAt(j - 1)){
-                sb.append(text2.charAt(j - 1));
-                i--;
-                j--;
-            }
-            else{
-                if(dp[i - 1][j] > dp[i][j - 1]){
-                    i--;
-                }
-                else{
-                    j--;
-                }
-            }
-        }
-        System.out.println(sb.reverse().toString());
-        return dp[m][n];
+        dp = new int[m][n];
+        for(int[] row: dp) Arrays.fill(row, -1);
+        return solve(text1, text2, m - 1, n - 1);
     }
-    // public int solve(String s, String p, int i, int j){
-    //     if(i < 0 || j < 0) return 0;
-    //     if(s.charAt(i) == p.charAt(j)){
-    //         return dp[i][j] = 1 + solve(s,p,i-1,j-1);
-    //     }
-    //     if(dp[i][j] != -1) return dp[i][j];
-    //     return dp[i][j] = Math.max(solve(s,p,i-1,j), solve(s,p,i,j-1));
-    // }
+    public int solve(String s, String p, int i, int j) {
+        if(i < 0 || j < 0) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s.charAt(i) == p.charAt(j)) {
+            return dp[i][j] =  1 + solve(s, p, i - 1, j - 1);
+        }
+
+        return dp[i][j] = Math.max(solve(s, p, i - 1, j), solve(s, p, i, j - 1));
+    }
 }
