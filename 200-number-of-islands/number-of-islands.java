325a087505e53e2b0ab05e3773059b576360256c
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-08-17 11:17:05 +0530

    Time: 4 ms (45.90%) | Memory: 52.5 MB (33.35%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -4,23 +4,24 @@ class Solution {
     public int numIslands(char[][] grid) {
         m = grid.length;
         n = grid[0].length;
-        int count = 0;
-        for(int i = 0; i < m ;i++){
-            for(int j = 0; j < n; j++){
-                if(grid[i][j] == '1'){
-                    dfs(grid,i,j);
-                    count++;
+        int islands = 0;
+        boolean[][] visited = new boolean[m][n];
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(grid[i][j] == '1' && !visited[i][j]) {
+                    dfs(grid, visited, i, j);
+                    islands++;
                 }
             }
         }
-        return count;
+        return islands;
     }
-    public void dfs(char[][] grid,int i,int j){
-        if(i < 0 || i >=m || j < 0 || j >=n ||grid[i][j] == '0') return;
-        grid[i][j] = '0';
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+    public void dfs(char[][] grid, boolean[][] visited, int i, int j) {
+        if(i >= m || i < 0 || j >= n || j < 0 || grid[i][j] == '0' || visited[i][j]) return;
+        visited[i][j] = true;
+        dfs(grid, visited, i + 1, j);
+        dfs(grid, visited, i - 1, j);
+        dfs(grid, visited, i, j + 1);
+        dfs(grid, visited, i, j - 1);
     }
 }
