bce5cd08158e9716f7847f4b08aac2b35e507727
Alisha Kapoor <alishakapoor0907@gmail.com>
2026-09-08 15:55:54 +0530

    Time: 1 ms (83.65%) | Memory: 42.5 MB (72.85%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.java
+++ 4245-count-commas-in-range/count-commas-in-range.java
@@ -2,9 +2,6 @@ class Solution {
     public int countCommas(int n) {
         int count = 0;
         if(n <= 999) return 0;
-        if(n == 100000) {
-            return (n - 1001) + 2;
-        }
         return n - 999;
     }
 }
